adfddc1d6d5ce76d2e588907183fda1270043a09
walt-instructor <staffauthor@outlook.com>
2023-10-06 13:28:55 -0700

    Fix curly braces

--- exercises/06-Config/ConsoleCSharp/Book.cs
+++ exercises/06-Config/ConsoleCSharp/Book.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleCSharp {
-  public class Book : Document{
-    // classes contain data and code
-    public string Title { get; set; }
-    public decimal Price { get; set; }
+namespace ConsoleCSharp
+{
+	public class Book : Document
+	{
+		// classes contain data and code
+		public string Title { get; set; }
+		public decimal Price { get; set; }
 
-    public long GetWordCount() {
-            // comments 
-            return 42;
-    }
-    public void SaveBook() {
-      // comments 
-      
-    }
-  }
+		public long GetWordCount()
+		{
+			// comments 
+			return 42;
+		}
+		public void SaveBook()
+		{
+			// comments 
 
-  public struct FileInformation {
-    // structs are primarily for data stores
-    public string FileName { get; set; }
-    public string Size { get; set; }
+		}
+	}
 
-    public event EventHandler<EventArgs> SizeChanged;
-  }
+	public struct FileInformation
+	{
+		// structs are primarily for data stores
+		public string FileName { get; set; }
+		public string Size { get; set; }
+
+		public event EventHandler<EventArgs> SizeChanged;
+	}
 }
--- exercises/06-Config/ConsoleCSharp/ExamplesHere.cs
+++ exercises/06-Config/ConsoleCSharp/ExamplesHere.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConsoleCSharp {
-	class ExamplesHere {
+namespace ConsoleCSharp
+{
+	class ExamplesHere
+	{
 		// classes contain data and code
 		public string Title { get; set; }
 		public decimal Price { get; set; }
 
-		public void ExampleMethod() {
+		public void ExampleMethod()
+		{
 			// comments 
 
 			// matching braces
-			if (true) {
-				for (int i = 0; i < 3; i++) {
-					switch (i) {
+			if (true)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					switch (i)
+					{
 						default:
 							break;
 					}
