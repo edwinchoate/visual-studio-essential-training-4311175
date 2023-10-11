50121272eccd18eda36c0ea3310c4d2dc0cce76b
Walt Ritscher <waltritscher@outlook.com>
2023-10-10 21:30:26 -0700

    Update .net references

--- exercises/09-Navigate/05-Peek/BookSellerWpf/BookListWindow.xaml.cs
+++ exercises/09-Navigate/05-Peek/BookSellerWpf/BookListWindow.xaml.cs
@@ -1,21 +1,8 @@
 ﻿using PublisherLib;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Linq;
 
-namespace BookSellerWpf
-{
+namespace BookSellerWpf {
 	/// <summary>
 	/// Interaction logic for BookListWindow.xaml
 	/// </summary>
