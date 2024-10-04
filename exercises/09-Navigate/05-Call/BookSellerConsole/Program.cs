b388a47b784ae64abb070f7d9f7eb379855168c2
Walt Ritscher <waltritscher@outlook.com>
2024-10-04 14:55:16 -0700

    update chapter numbers

---
+++ exercises/09-Navigate/05-Call/BookSellerConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using PublisherLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSellerConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dataSource = new PublisherLib.Data.BookDataSource();
+			PublisherLib.Book book = dataSource.GetBestSellingBook();
+			Console.WriteLine($"Title: {book.Title}, Copies Sold:{book.CopiesSold}");
+			
+			// make a method call to a class
+			book.UpdatePrice(48.95M);
+
+		}
+	}
+}
