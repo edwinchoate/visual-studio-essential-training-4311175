2e4ce5d91ef449519d9b0ed3454b5d621530a49d
Walt Ritscher <waltritscher@outlook.com>
2024-10-10 11:47:58 -0700

    Add chapter 11

---
+++ exercises/11-Continue/SourceControl/BookSellerWpf/BookListWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PublisherLib;
+using System.Linq;
+using System.Windows;
+
+namespace BookSellerWpf {
+	/// <summary>
+	/// Interaction logic for BookListWindow.xaml
+	/// </summary>
+	public partial class BookListWindow : Window
+	{
+		public BookListWindow()
+		{
+			InitializeComponent();
+
+			var dataSource = new PublisherLib.Data.BookDataSource();
+			var books = dataSource.GetAllBooks().OrderBy(x => x.Title);
+			this.DataContext = books;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var book = BooksListBox.SelectedItem as Book;
+			book.UpdatePrice(22.50M);
+		}
+	}
+}
